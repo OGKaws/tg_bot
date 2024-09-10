73003bacc32cce096a9488eeb45ca095673df4be
Nikita Usachev <usachevnv@mail.ru>
2024-09-10 20:16:19 +0300

    new_commit

--- main_bot.py
+++ main_bot.py
@@ -10,7 +10,7 @@ import telebot
 from telebot import types
 
 ########################################################################################################################-------- ПЕРЕМЕННЫЕ
-
+# git_test
 bot = telebot.TeleBot('7073557228:AAHxo15uAsWyGQ7sRvv8POBqGjX1yNlHFf0')
 users = Connected_Users()
 # client_q = 0
